5dc35dce4f5b74b66c718f0a20e4c682ff262556
YuliaLeo <89121084+YuliaLeo@users.noreply.github.com>
2023-01-27 15:30:54 +0700

    Types added

---
+++ src/app/types/ContentKinds.ts
@@ -0,0 +1,4 @@
+export type TContentKind = 'youtube#videoListResponse'; // что тут лучше литеральный тип или enum?
+//export const enum TContentKind {
+//	videoList = 'youtube#videoListResponse'
+//}
