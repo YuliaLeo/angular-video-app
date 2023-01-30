6f19cded11213b7d9aebcdf4b0ae608a799bf618
YuliaLeo <89121084+YuliaLeo@users.noreply.github.com>
2023-01-30 10:49:43 +0700

    Routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VideoDashboardComponent } from './components/video-dashboard/video-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', redirectTo: '/videos', pathMatch: 'full' },
+	{ path: 'videos', component: VideoDashboardComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
