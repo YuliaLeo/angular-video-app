81bfa1404b4edc4eead187f1f9833f7fc847a97c
YuliaLeo <89121084+YuliaLeo@users.noreply.github.com>
2023-01-30 10:36:20 +0700

    URLSearchParams interface used

--- src/app/services/video.service.ts
+++ src/app/services/video.service.ts
@@ -2,18 +2,21 @@ import { Injectable } from '@angular/core';
 import { Observable, map } from "rxjs";
 import { HttpClient } from "@angular/common/http";
 import { IVideo } from '../types/Video';
-import { IResponse } from '../types/VideoResponse';
+import { IVideoResponse } from '../types/VideoResponse';
+import { ISearchParams } from '../types/SearchParams';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class VideoService {
-	// это нормально так собирать ссылку?
 	private videosUrl = 'https://youtube.googleapis.com/youtube/v3/videos';
-	private part = 'snippet';
-	private chart = 'mostPopular';
-	private apiKey = 'AIzaSyAGbTOEseMlTZ91Oz1q9Rh3vWxMoYiZADE';
-	private videoSource = `${this.videosUrl}?part=${this.part}&chart=${this.chart}&key=${this.apiKey}`;
+	private searchParams: ISearchParams = {
+		part: 'snippet',
+		chart: 'mostPopular',
+		key: 'AIzaSyAGbTOEseMlTZ91Oz1q9Rh3vWxMoYiZADE'
+	}
+	private searchParamsString = new URLSearchParams(this.searchParams).toString();
+	private videosSource = `${this.videosUrl}?${this.searchParamsString}`;
 
 	videos: IVideo[] = [];
 
@@ -21,7 +24,7 @@ export class VideoService {
 	}
 
 	getVideos(): Observable<IVideo[]> {
-		return this.http.get<IResponse>(this.videoSource)
-			.pipe(map((response) => this.videos = response.items))
+		return this.http.get<IVideoResponse>(this.videosSource)
+			.pipe(map(response => this.videos = response.items));
 	}
 }
