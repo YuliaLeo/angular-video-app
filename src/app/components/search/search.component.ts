a7c43a465a1416c2201acd6ce04f1d22e16d7083
YuliaLeo <89121084+YuliaLeo@users.noreply.github.com>
2023-01-31 18:39:57 +0700

    Video search added

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subject, debounceTime, distinctUntilChanged, switchMap } from "rxjs";
+import { VideoService } from 'src/app/services/video.service';
 
 @Component({
 	selector: 'app-search',
@@ -6,4 +8,20 @@ import { Component } from '@angular/core';
 	styleUrls: ['./search.component.scss']
 })
 export class SearchComponent {
+
+	constructor(private videoService: VideoService) { }
+
+	searchTerms = new Subject<string>();
+
+	search(term: string): void {
+		this.searchTerms.next(term);
+	}
+
+	ngOnInit(): void {
+		this.searchTerms.pipe(
+			debounceTime(300),
+			distinctUntilChanged(),
+			switchMap((term: string) => this.videoService.searchVideos(term)),
+		).subscribe();
+	}
 }
--- src/app/services/video.service.ts
+++ src/app/services/video.service.ts
@@ -3,20 +3,19 @@ import { Observable, map } from "rxjs";
 import { HttpClient } from "@angular/common/http";
 import { IVideo } from '../types/Video';
 import { IVideoResponse } from '../types/VideoResponse';
-import { ISearchParams } from '../types/SearchParams';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class VideoService {
-	private videosUrl = 'https://youtube.googleapis.com/youtube/v3/videos';
-	private searchParams: ISearchParams = {
+	private videosUrl = 'https://youtube.googleapis.com/youtube/v3/videos?';
+	private searchParamsForMultipleVideos = new URLSearchParams({
 		part: 'snippet',
 		chart: 'mostPopular',
-		key: 'AIzaSyAGbTOEseMlTZ91Oz1q9Rh3vWxMoYiZADE'
-	}
-	private searchParamsString = new URLSearchParams(this.searchParams).toString();
-	private videosSource = `${this.videosUrl}?${this.searchParamsString}`;
+		key: 'AIzaSyAGbTOEseMlTZ91Oz1q9Rh3vWxMoYiZADE',
+		maxResults: '12'
+	});
+	private videosSource = this.videosUrl + this.searchParamsForMultipleVideos.toString();
 
 	videos: IVideo[] = [];
 
@@ -26,5 +25,14 @@ export class VideoService {
 	getVideos(): Observable<IVideo[]> {
 		return this.http.get<IVideoResponse>(this.videosSource)
 			.pipe(map(response => this.videos = response.items));
+	}
+
+	searchVideos(term: string): Observable<IVideo[]> {
+		return this.http.get<IVideoResponse>(this.videosSource)
+			.pipe(
+				map(response => response.items
+					.filter(item => item.snippet.title.toLowerCase().includes(term))),
+				map(matchedVideos => this.videos = matchedVideos)
+			);
 	}
 }
